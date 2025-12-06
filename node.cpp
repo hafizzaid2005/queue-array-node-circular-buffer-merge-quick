df9e63d35197fc4db6f5ba47073f9693056c574f
M.ZAID <hanzalazaidkhalid@gmail.com>
2025-12-06 19:20:10 +0500

    Define Node class with data and next pointers
    Implemented constructors, destructors, and accessor methods for the Node class.

--- node.cpp
+++ node.cpp
@@ -1,8 +1,35 @@
 #include "node.h"
 
-Node::Node() {}
+Node::Node() 
+{
+    data = 0;
+    next = NULL;
+}
+
 Node::~Node() {}
-Node::Node(const int data) {}
-int Node::getData() { return 0; }
-Node* Node::getNext() {}
-void Node::setData(const int data) {}
+
+Node::Node(const int data)
+{
+    this->data = data;
+    this->next = NULL;
+}
+
+int Node::getData()
+{
+    return data;
+}
+
+Node* Node::getNext()
+{
+    return next;
+}
+
+void Node::setData(const int data)
+{
+    this->data = data;
+}
+
+void Node::setNext(Node* next)
+{
+    this->next = next;
+}
