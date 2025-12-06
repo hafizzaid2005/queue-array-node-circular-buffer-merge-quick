0293f93eb57409c7b241e4797ce408969edcc383
M.ZAID <hanzalazaidkhalid@gmail.com>
2025-12-06 15:26:19 +0500

    Implement ArrayBasedQueue methods and constructor

--- ab_queue.cpp
+++ ab_queue.cpp
@@ -1,13 +1,111 @@
 #include "queue.h"
 
-ArrayBasedQueue::ArrayBasedQueue(const int capacity) {}
-ArrayBasedQueue::ArrayBasedQueue(const ArrayBasedQueue& other) {}
-ArrayBasedQueue& ArrayBasedQueue::operator = (const ArrayBasedQueue& other) { return *this; }
-ArrayBasedQueue::~ArrayBasedQueue() {}
+ArrayBasedQueue::ArrayBasedQueue(const int capacity)
+{
+    this->capacity = capacity;
+    queue = new int[capacity];
+    front = -1;
+    rear = -1;
+}
 
-void ArrayBasedQueue::enqueue (const int data) {}
-int ArrayBasedQueue::dequeue () { return 0; }
-int ArrayBasedQueue::peek() const { return 0; }
+ArrayBasedQueue::ArrayBasedQueue(const ArrayBasedQueue& other)
+{
+    capacity = other.capacity;
+    queue = new int[capacity];
+    front = other.front;
+    rear = other.rear;
 
-bool ArrayBasedQueue::isEmpty() const { return true; }
-int ArrayBasedQueue::getSize() const { return 0; }
+    for (int i = 0; i < capacity; i++)
+        queue[i] = other.queue[i];
+}
+
+ArrayBasedQueue& ArrayBasedQueue::operator = (const ArrayBasedQueue& other)
+{
+    if (this != &other)
+    {
+        delete[] queue;
+        capacity = other.capacity;
+        queue = new int[capacity];
+        front = other.front;
+        rear = other.rear;
+
+        for (int i = 0; i < capacity; i++)
+            queue[i] = other.queue[i];
+    }
+    return *this;
+}
+
+ArrayBasedQueue::~ArrayBasedQueue()
+{
+    delete[] queue;
+    queue = NULL;
+    front = rear = -1;
+    capacity = 0;
+}
+
+void ArrayBasedQueue::enqueue(const int data)
+{
+    if (front == -1 && rear == -1)
+    {
+        front = rear = 0;
+        queue[rear] = data;
+        return;
+    }
+
+    int nextRear = (rear + 1) % capacity;
+
+    if (nextRear == front)
+    {
+        std::cout << "Overflow" << std::endl;
+        return;
+    }
+
+    rear = nextRear;
+    queue[rear] = data;
+}
+
+int ArrayBasedQueue::dequeue()
+{
+    if (front == -1)
+    {
+        std::cout << "Underflow" << std::endl;
+        return -1;
+    }
+
+    int val = queue[front];
+
+    if (front == rear)
+    {
+        front = rear = -1;
+    }
+    else
+    {
+        front = (front + 1) % capacity;
+    }
+
+    return val;
+}
+
+int ArrayBasedQueue::peek() const
+{
+    if (front == -1)
+        return -1;
+
+    return queue[front];
+}
+
+bool ArrayBasedQueue::isEmpty() const
+{
+    return (front == -1);
+}
+
+int ArrayBasedQueue::getSize() const
+{
+    if (front == -1)
+        return 0;
+
+    if (rear >= front)
+        return (rear - front + 1);
+
+    return (capacity - front) + (rear + 1);
+}
